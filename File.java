2de7754159901389b15296afacabcbe94bf7f0cf
Kaciade <valerypolyakov60@gmail.com>
2023-08-24 16:25:27 +0200

    1

---
+++ File.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+ 
+public class File
+{
+    private static int LEFT(int i) {
+        return (2*i + 1);
+    }
+ 
+    private static int RIGHT(int i) {
+        return (2*i + 2);
+    }
+}
